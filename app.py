f575d1821b3a5fb67cb274d791f11bb9f103f975
Comunicando Igrejas <comunicandoigrejas@gmail.com>
2026-02-21 16:45:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
+from streamlit_gsheets 
+import GSheetsConnection
 import pandas as pd
 
 # Configuração visual - Cores Comunicando Igrejas
